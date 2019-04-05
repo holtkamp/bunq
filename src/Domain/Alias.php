945a2623c85285507f6e8a94465dc641cf0b8dd9
Holtkamp <menno.holtkamp@shopforce.nl>
2019-04-05 10:32:29 +0200

    Improve dealing with Aliases

--- src/Domain/Alias.php
+++ src/Domain/Alias.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Link0\Bunq\Domain;
 
@@ -6,9 +6,10 @@ use InvalidArgumentException;
 
 final class Alias
 {
-    const TYPE_PHONE_NUMBER = 'PHONE_NUMBER';
     const TYPE_EMAIL = 'EMAIL';
     const TYPE_IBAN = 'IBAN';
+    const TYPE_PHONE_NUMBER = 'PHONE_NUMBER';
+    const TYPE_URL = 'URL';
 
     /**
      * @var string
@@ -25,30 +26,32 @@ final class Alias
      */
     private $name;
 
-    /**
-     * @param string $type
-     * @param string $value
-     * @param string $name
-     */
     public function __construct(string $type, string $value, string $name)
     {
         $this->guardValidType($type);
 
-        $this->type = $type;
+        $this->type  = $type;
         $this->value = $value;
-        $this->name = $name;
+        $this->name  = $name;
     }
 
-    public static function fromArray(array $alias)
+    public static function fromArray(array $alias): ?self
     {
-        return new Alias(
-            $alias['type'],
-            $alias['value'],
-            $alias['name']
-        );
+        try {
+            return new self(
+                $alias['type'],
+                $alias['value'],
+                $alias['name']
+            );
+        } catch (\Exception $exception) {
+            \error_log('caught exception while trying to assemble Alias: ' . $exception->getMessage());
+        }
+
+
+        return null;
     }
 
-    public function toArray()
+    public function toArray(): array
     {
         return [
             'type' => $this->type(),
@@ -58,41 +61,26 @@ final class Alias
     }
 
     /**
-     * @param string $type
-     * @return void
      * @throws InvalidArgumentException
      */
-    private function guardValidType(string $type)
+    private function guardValidType(string $type): void
     {
-        switch ($type) {
-            case self::TYPE_PHONE_NUMBER:
-            case self::TYPE_EMAIL:
-            case self::TYPE_IBAN:
-                break;
-            default:
-                throw new InvalidArgumentException("Invalid Alias type: " . $type);
+        $reflectionClass = new \ReflectionClass(__CLASS__);
+        if (!array_key_exists('TYPE_' . $type, $reflectionClass->getConstants())) {
+            throw new \InvalidArgumentException(sprintf('Invalid Alias type "%s"', $type));
         }
     }
 
-    /**
-     * @return string
-     */
     public function type(): string
     {
         return $this->type;
     }
 
-    /**
-     * @return string
-     */
     public function value(): string
     {
         return $this->value;
     }
 
-    /**
-     * @return string
-     */
     public function name(): string
     {
         return $this->name;
